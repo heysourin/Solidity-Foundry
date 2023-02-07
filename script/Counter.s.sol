37435b8c585df8609063ed93834181d4c5011bfe
heysourin <sourin07@gmail.com>
2023-02-07 14:07:19 +0530

    Updated

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,11 +2,18 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
-    function run() public {
-        vm.broadcast();
+    function run() external {
+        // uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast();
+        // Counter counter = new Counter(55);
+        new Counter(12);
+        vm.stopBroadcast();
     }
 }
+
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -7,7 +7,24 @@ import "../src/Counter.sol";
 contract ContractTest is Test {
     Counter public myContract;
 
-    function testExample() public {
-        assertTrue(true);        
+    // function testExample() public {
+    //     assertTrue(true);
+    // }
+
+    function setUp() public {
+        myContract = new Counter(10);
+    }
+
+    function testGetCount() public {
+        int value = myContract.getCount();
+        assertEq(value, 10);
+        emit log_named_int("The value is", value); //When you want to print
+    }
+
+    function testIncrementCounter() public {
+        myContract.incrementCounter();
+        int value = myContract.getCount();
+        assertEq(value, 11);
+        emit log_named_int("The value is", value);
     }
 }
